087e0f5ff20817f2f00f62b361b11898b0617eec
Sidath Weerasinghe <sidath@wso2telco.com>
2017-02-13 13:12:58 +0530

    Recorrect Code

--- dep-sandbox/src/main/java/com/wso2telco/services/dep/sandbox/dao/hibernate/HibernatePaymentDAO.java
+++ dep-sandbox/src/main/java/com/wso2telco/services/dep/sandbox/dao/hibernate/HibernatePaymentDAO.java
@@ -1,18 +1,18 @@
-/*
- * Copyright (c) 2017, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+/*******************************************************************************
+ * Copyright (c) 2015-2017, WSO2.Telco Inc. (http://www.wso2telco.com)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * All Rights Reserved. WSO2.Telco Inc. licences this file to you under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ ******************************************************************************/
 
 package com.wso2telco.services.dep.sandbox.dao.hibernate;
 
@@ -136,7 +136,7 @@ public class HibernatePaymentDAO extends AbstractDAO implements PaymentDAO {
             saveOrUpdate(valueObj);
             values = valueObj.getAttributeValueId();
         } catch (Exception e) {
-            LOG.error("saveAttributeValue", e);
+            LOG.error("###PAYMENT### Error in save AttributeValue", e);
             throw e;
         }
         return values;
